fdfec72d9486e011c5b976ed3da018d0a68eb8aa
alfonso <alfonsolasaguirre@gmail.com>
2026-04-24 11:55:43 +0200

    fix process env placeholder

--- vite.web-component.config.ts
+++ vite.web-component.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   define: {
     global: 'globalThis',
+    'process.env.NODE_ENV': JSON.stringify('production'),
   },
   plugins: [react()],
   build: {
